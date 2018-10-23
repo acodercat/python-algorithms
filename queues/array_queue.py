e3482260e02c50a95391ec86d3c5c6b937e7f4e3
acodercat <1067302838@qq.com>
2018-10-23 17:40:20 +0800

    add ArrayQueue
    add ArrayQueue

---
+++ queues/array_queue.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class ArrayQueue:
+
+    def __init__(self, capacity):
+        assert capacity > 0
+        self.__capacity = capacity
+        self.__arr = [None] * capacity
+        self.__count = 0
+
+    def enqueue(self, val):
+        assert not self.isFull()
+        self.__arr[self.getSize()] = val
+        self.__count = self.__count + 1
+
+    def dequeue(self):
+        assert not self.isEmpty()
+        front = self.getFront()
+        for i in range(1, self.getSize()):
+            self.__arr[i - 1] = self.__arr[i]
+        self.__count = self.__count - 1
+        return front
+
+    def isFull(self):
+        return self.__count >= self.__capacity
+
+    def isEmpty(self):
+        return self.__count == 0
+
+    def getSize(self):
+        return self.__count
+
+    def getFront(self):
+        return self.__arr[0]
+
+
+
+arrayQueue = ArrayQueue(5)
+arrayQueue.enqueue(1)
+arrayQueue.enqueue(2)
+arrayQueue.enqueue(3)
+front = arrayQueue.getFront()
+print(front)
+
